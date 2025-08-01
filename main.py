2bd5670f846fcfbe713d2c7977eed3caee035d6d
mihahafner <63566666+mihahafner@users.noreply.github.com>
2025-08-01 11:24:35 +0000

    Koda za main

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-refgregerg
+from csv_writer import write_csv
+from helper import welcome
+
+def main():
+    welcome("Jupyter User")
+    write_csv("sample_output.csv")
+
+if __name__ == "__main__":
+    main()
+🐍 csv_writer.py
